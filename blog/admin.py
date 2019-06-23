d537be3932beafa157f6a9f100e1d7c7af30b717
Daianes26 <daianeasilva26@gmail.com>
2019-06-23 18:22:13 -0400

    Second commit. Blog models and registration

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from blog.models import Blog
+from blog.models import Blog, BlogAuthor, BlogComment
 
 # Register your models here.
 admin.site.register(Blog)
+admin.site.register(BlogAuthor)
+admin.site.register(BlogComment)
--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class BlogAuthor(models.Model):
+    """Model representing the blogger"""
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(max_length=300)
+
+    class Meta:
+        ordering = ["user", "bio"]
+    
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.user.username
+
 
 class Blog(models.Model):
     """ Model for the blog post """
     title = models.CharField(max_length=200)
-   
+    author= models.ForeignKey(BlogAuthor, on_delete=models.SET_NULL, null=True)
     blog_post = models.TextField()
-    post_date = models.DateField(auto_now_add=True)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title', 'post_date']
 
     def __str__(self):
-        return self.text
+        return self.title
+
+
+class BlogComment(models.Model):
+    """ Model representing blog comments"""
+   
+    comment = models.TextField(max_length=600)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['post_date']
+
+    def __str__(self):
+
+        if len(self.comment) > 50:
+            return self.comment[:50] + "..."
+        else:
+            return self.comment
